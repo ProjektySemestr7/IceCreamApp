b95ea5687470386ce3f9ad6c661a096ca8317d88
caco7 <caco7.lioheart@gmail.com>
2021-11-21 19:07:00 +0100

    closed #6 Added list of icecream

---
+++ lib/components/store_info_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StoreInfoBar extends StatelessWidget {
+  const StoreInfoBar({Key? key, required this.storeId}) : super(key: key);
+  final String storeId;
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference stores =
+        FirebaseFirestore.instance.collection('Stores');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: stores.doc(storeId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          var xd = snapshot.data;
+
+          return Container(
+            decoration: const BoxDecoration(color: Colors.lightGreen),
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      data['Name'],
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      data['Address'],
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                FloatingActionButton(
+                    onPressed: () {}, child: const Icon(Icons.navigation)),
+              ],
+            ),
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/views/store_details.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ice2/components/store_info_bar.dart';
+
+class StoreDetails extends StatefulWidget {
+  StoreDetails({Key? key, required this.storeId}) : super(key: key);
+
+  String storeId;
+
+  @override
+  _StoreDetailsState createState() => _StoreDetailsState();
+}
+
+class _StoreDetailsState extends State<StoreDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Expanded(
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Stores')
+                  .doc(widget.storeId)
+                  .collection("Icecreams")
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  return ListView(
+                    children: snapshot.data!.docs.map((document) {
+                      return Card(
+                          child: ListTile(
+                        title: Text(document.get("Name")),
+                        subtitle:
+                            Text(document.get("Price").toString() + " PLN"),
+                      ));
+                    }).toList(),
+                  );
+                }
+              }),
+        ),
+        StoreInfoBar(storeId: widget.storeId)
+      ]),
+    );
+  }
+}
