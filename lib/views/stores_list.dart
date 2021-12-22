d96a0e5c578daad22da377a9a184218a92bbd27a
Pawxon <zelazek_pawel@wp.pl>
2021-12-22 14:13:27 +0100

    closed #14 Add search for ice cream stores

--- lib/views/stores_list.dart
+++ lib/views/stores_list.dart
@@ -10,47 +10,72 @@ class StoresList extends StatefulWidget {
 }
 
 class _StoresListState extends State<StoresList> {
+  String search = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-        ),
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Center(
-          child: StreamBuilder(
-              stream:
-                  FirebaseFirestore.instance.collection('Stores').snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                //Map<String, dynamic> xd = snapshot.data as Map<String, dynamic>;
-                if (!snapshot.hasData ||
-                    snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  return ListView(
-                    children: snapshot.data!.docs.map((document) {
-                      return Card(
-                          child: ListTile(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      StoreDetails(storeId: document.id)));
-                        },
-                        title: Text(document.get("Name")),
-                        subtitle: Text(document.get("City")),
-                      ));
-                    }).toList(),
-                  );
-                }
-              }),
-        ),
+      body: Column(
+        children: [
+          Container(
+              child: TextField(
+                  onChanged: (String text) => search = text,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Wpisz nazwę lodziarni, albo miejcowość')),
+              padding: const EdgeInsets.only(top: 50)),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('Stores')
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData ||
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else {
+                        return ListView(
+                          children: snapshot.data!.docs.map((document) {
+                            String name =
+                                document.get("Name").toString().toLowerCase();
+                            String city =
+                                document.get("City").toString().toLowerCase();
+
+                            if (name.contains(search.toLowerCase()) ||
+                                city.contains(search.toLowerCase())) {
+                              return Card(
+                                  child: ListTile(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => StoreDetails(
+                                              storeId: document.id)));
+                                },
+                                title: Text(document.get("Name")),
+                                subtitle: Text(document.get("City")),
+                              ));
+                            } else {
+                              return Container();
+                            }
+                          }).toList(),
+                        );
+                      }
+                    }),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
